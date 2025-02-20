7ce8a6594b8c229d2f0673f645f8efb0d14f46da
Marta Zigure <121212689+MartaAija@users.noreply.github.com>
2025-02-20 06:45:48 +0000

    main.py

--- backend/main.py
+++ backend/main.py
@@ -402,4 +402,5 @@ def forgot_password():
             db.close()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
